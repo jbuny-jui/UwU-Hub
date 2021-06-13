f5b7181c55799484e1a09c87c524af6e967b11a1
jbuny-jui <81629958+jbuny-jui@users.noreply.github.com>
2021-06-13 15:00:52 -0400

    Update Hitbox extender.lua

--- Hitbox extender.lua
+++ Hitbox extender.lua
@@ -5,6 +5,7 @@ return v.Name
 end
 end
 end
+hitboxsizevar = Vector3.new(3,3,3)
 local players = getplrsname()
 local plr = game[players].LocalPlayer
 coroutine.resume(coroutine.create(function()
@@ -14,19 +15,17 @@ for _,v in pairs(game[players]:GetPlayers()) do
 if v.Name ~= plr.Name and v.Character then
 v.Character.RightUpperLeg.CanCollide = false
 v.Character.RightUpperLeg.Transparency = 10
-v.Character.RightUpperLeg.Size = Vector3.new(3,3,3)
-
+v.Character.RightUpperLeg.Size = hitboxsizevar
 v.Character.LeftUpperLeg.CanCollide = false
 v.Character.LeftUpperLeg.Transparency = 10
-v.Character.LeftUpperLeg.Size = Vector3.new(3,3,3)
-
+v.Character.LeftUpperLeg.Size = hitboxsizevar
 v.Character.HeadHB.CanCollide = false
 v.Character.HeadHB.Transparency = 10
-v.Character.HeadHB.Size = Vector3.new(3,3,3)
+v.Character.HeadHB.Size = hitboxsizevar
 
 v.Character.HumanoidRootPart.CanCollide = false
 v.Character.HumanoidRootPart.Transparency = 10
-v.Character.HumanoidRootPart.Size = Vector3.new(3,3,3)
+v.Character.HumanoidRootPart.Size = hitboxsizevar
 
 end
 end
