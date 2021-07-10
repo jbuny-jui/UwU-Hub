c1ac0247195dacba431e8b04a3c123fe4a6ea133
jbuny-jui <81629958+jbuny-jui@users.noreply.github.com>
2021-07-09 21:22:45 -0400

    Create Regular.lua

---
+++ Regular.lua
@@ -0,0 +1,3 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/jbuny-jui/UwU-Hub/main/Project_Bullshit.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/jbuny-jui/UwU-Hub/main/triggerbot.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/jbuny-jui/UwU-Hub/main/Hitbox%20extender.lua"))()
